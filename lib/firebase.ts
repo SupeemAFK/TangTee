adce0d80a3cb834927a0133e8bd8a2b45034656c
mina <peemlomsak@gmail.com>
2023-09-24 02:18:23 +0700

    add env to firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKctYMVHNAbKmUymsjq_tSxaB0gU07eJk",
-    authDomain: "tangtee-d1030.firebaseapp.com",
-    projectId: "tangtee-d1030",
-    storageBucket: "tangtee-d1030.appspot.com",
-    messagingSenderId: "230155681774",
-    appId: "1:230155681774:web:ac72f7277d5677488298ff",
-    measurementId: "G-1BN7BRTGHF"
+    apiKey: process.env.NEXT_PUBLIC_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
